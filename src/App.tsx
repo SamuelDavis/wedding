cde672d6ea3f1406a6d9f5758acde4c119d97759
Samuel Davis <samueljakdavis@gmail.com>
2025-11-28 15:29:40 -0500

    draft your-trip page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { lazy } from "solid-js";
 const Layout = lazy(() => import("./Pages/Layout"));
 const Home = lazy(() => import("./Pages/Home"));
 const OurStory = lazy(() => import("./Pages/OurStory"));
+const YourTrip = lazy(() => import("./Pages/YourTrip"));
 const NotFound = lazy(() => import("./Pages/NotFound"));
 
 export default function App() {
@@ -11,6 +12,7 @@ export default function App() {
     <Router root={Layout}>
       <Route path="/" component={Home} />
       <Route path="/our-story" component={OurStory} />
+      <Route path="/your-trip" component={YourTrip} />
       <Route path="*404" component={NotFound} />
     </Router>
   );
---
+++ src/Pages/YourTrip.tsx
@@ -0,0 +1,54 @@
+export default function YourTrip() {
+  return (
+    <>
+      <section>
+        <header>
+          <h2>Your Trip</h2>
+          <img src="https://placehold.co/600x600" />
+        </header>
+      </section>
+      <section>
+        <header>
+          <h2>Getting Here</h2>
+        </header>
+        <div>
+          Charlotte Douglas
+          <br />
+          Internation Airport
+        </div>
+      </section>
+      <section>
+        <header>
+          <h2>Where to Stay</h2>
+        </header>
+        <header>
+          <img src="https://placehold.co/200x100" />
+          <h3>Wingate by Wyndham</h3>
+        </header>
+        <a target="_blank" href="https://maps.app.goo.gl/Q6MRJ6W2utcNVrDC8">
+          <address>
+            2001 Mt. Harmony Church Rd.
+            <br />
+            Matthews, NC 28105
+          </address>
+        </a>
+        <p>
+          When you cal, mention that you want a room for Samuel & Jessica's
+          wedding.
+        </p>
+        <a
+          target="_blank"
+          href="https://www.wyndhamhotels.com/en-ca/wingate/matthews-north-carolina/wingate-matthews-charlotte/overview"
+        >
+          Book
+        </a>
+      </section>
+      <section>
+        <header>
+          <h2>Discover Charlotte</h2>
+        </header>
+        <p>Explore what you can do while you're here!</p>
+      </section>
+    </>
+  );
+}
