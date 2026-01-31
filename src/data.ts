eafc9e62f6f1d8c53ef535d8f43fae95fcfca24a
Samuel Davis <samueljakdavis@gmail.com>
2026-01-31 17:02:26 -0500

    safari compliance

--- src/data.ts
+++ src/data.ts
@@ -1,12 +1,12 @@
-import { set } from "date-fns";
+import { parseISO, set } from "date-fns";
 import timeline2025Src from "./assets/SamandJess_beach.jpg";
 import timeline2024Src from "./assets/SamandJess_capitalgrille.jpg";
 import timeline2023Src from "./assets/SamandJess_athens-color.jpg";
 import timeline2022Src from "./assets/SamandJess_outside.jpg";
 import timeline2021Src from "./assets/SamandJess_car.jpg";
 
-export const weddingDate = new Date("2026-05-31 00:00:00 GMT-0400");
-export const rsvpDate = new Date("2026-04-01 00:00:00 GMT-0400");
+export const weddingDate = parseISO("2026-05-31T00:00:00-04:00");
+export const rsvpDate = parseISO("2026-04-01T00:00:00-04:00");
 export const venueMapHref = "https://maps.app.goo.gl/RyRjoQsDTzFjBgZN6";
 export const hotelMapHref = "https://maps.app.goo.gl/Q6MRJ6W2utcNVrDC8";
 export const hotelSiteHref =
