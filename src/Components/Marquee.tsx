0791f0a13709f524d22a1622ca7b858095896628
Samuel Davis <samueljakdavis@gmail.com>
2026-02-09 22:54:22 -0500

    safari support for responsive image sizing?

--- src/Components/Marquee.tsx
+++ src/Components/Marquee.tsx
@@ -5,11 +5,13 @@ import ImgAsset from "./ImgAsset";
 export default function Marquee(props: ExtendProps<"ul", { srcs?: string[] }>) {
   const [local, parent] = splitProps(props, ["children", "srcs"]);
   const getChildren = () =>
-    local.srcs?.map((src) => <ImgAsset src={src} />) ??
+    local.srcs?.map((src) => <ImgAsset src={src} class="w-full" />) ??
     children(() => local.children).toArray();
   return (
     <ul role="marquee" {...parent}>
-      <For each={getChildren()}>{(child) => <li>{child}</li>}</For>
+      <For each={getChildren()}>
+        {(child) => <li class="w-full h-full">{child}</li>}
+      </For>
     </ul>
   );
 }
