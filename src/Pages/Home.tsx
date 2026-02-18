94c64ea493d059b752730171e31f8d493f231e09
Samuel Davis <samueljakdavis@gmail.com>
2026-02-18 17:34:55 -0500

    redesign additional details

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -15,7 +15,6 @@ import flowersColorSrc from "../assets/SamandJess_flowers-color.jpg";
 import arrivalAndCeremonySrc from "../assets/SamandJess_Hydrangeas.jpg";
 import socialHourSrc from "../assets/SamandJess_FrancisBeattyPark.jpg";
 import receptionAndDinnerSrc from "../assets/SamandJess_dancing_BW.jpg";
-import additionalDetailsSrc from "../assets/SamandJess_lace.jpg";
 import ImgAsset from "../Components/ImgAsset";
 import { A } from "@solidjs/router";
 import Arrow from "../Components/Arrow";
@@ -59,7 +58,10 @@ export default function Home() {
           </menu>
         </nav>
       </section>
-      <section id="our-story" class="flex flex-col items-center text-center">
+      <section
+        id="our-story"
+        class="bg-light flex flex-col items-center text-center"
+      >
         <h1 class="mb-(--gap-sm)">
           <A id="our-story" href="#our-story">
             <b>Let's Make This Official</b>
@@ -193,50 +195,31 @@ export default function Home() {
           <ImgAsset src={receptionAndDinnerSrc} class="border" />
         </section>
       </section>
-      <section id="details" class="cols-2 border mx-(--gap-main) py-(--gap-md)">
-        <figure class="relative">
-          <figcaption class="absolute top-1 left-1 sm:-left-8">
-            <h1>
-              <A href="#details">
-                <em>
-                  Additional
-                  <br />
-                  Details
-                </em>
-              </A>
-            </h1>
-          </figcaption>
-          <ImgAsset src={additionalDetailsSrc} />
-        </figure>
-        <ul>
-          <li>
+      <section id="details" class="bg-light mx-(--gap-main) py-(--gap-md)">
+        <h1 class="flex justify-center">
+          <A href="#details">
+            <em>Additional Details</em>
+          </A>
+        </h1>
+        <ul class="grid gap-(--gap-md) br:grid-cols-3">
+          <li class="text-center">
             <h1>Attire</h1>
             <p>
               Bright, nature-inspired colors are encouraged. The ceremony is
               taking place outdoors, so dress for the weather!
-            </p>
-            <p>
-              <span>View attire examples </span>
-              <a target="_blank" href={attireHref}>
-                here
-              </a>
-              <span>.</span>
+              <span class="block mt-2">
+                View attire examples <a href={attireHref}>here</a>.
+              </span>
             </p>
           </li>
-          <li>
-            <hr />
-          </li>
-          <li>
+          <li class="text-center">
             <h1>Little Ones</h1>
             <p>
               We are only able to accommodate children listed on your invite.
               Enjoy your night off!
             </p>
           </li>
-          <li>
-            <hr />
-          </li>
-          <li>
+          <li class="text-center">
             <h1>Gifts</h1>
             <p>
               Your presence is the real gift! If you'd still like to give, a
