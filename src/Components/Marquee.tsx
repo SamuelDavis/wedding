66112021506f08b7adab2427c0ce2fa299814631
Samuel Davis <samueljakdavis@gmail.com>
2026-02-20 19:39:21 -0500

    add custom scrollbar

--- src/Components/Marquee.tsx
+++ src/Components/Marquee.tsx
@@ -1,5 +1,12 @@
-import type { ExtendProps } from "@samueldavis/solidlib";
-import { children, For, splitProps } from "solid-js";
+import { type ExtendProps, type Targeted } from "@samueldavis/solidlib";
+import {
+  children,
+  createEffect,
+  createSignal,
+  For,
+  onCleanup,
+  splitProps,
+} from "solid-js";
 import ImgAsset from "./ImgAsset";
 
 export default function Marquee(props: ExtendProps<"ul", { srcs?: string[] }>) {
@@ -7,9 +14,50 @@ export default function Marquee(props: ExtendProps<"ul", { srcs?: string[] }>) {
   const getChildren = () =>
     local.srcs?.map((src) => <ImgAsset src={src} />) ??
     children(() => local.children).toArray();
+
+  let el!: HTMLUListElement;
+  const [getMax, setMax] = createSignal(0);
+  const [getVal, setVal] = createSignal(0);
+
+  function onScroll() {
+    const max = Math.max(0, el.scrollWidth - el.clientWidth);
+    setMax(max);
+    setVal(Math.min(max, el.scrollLeft));
+  }
+
+  function onInput(event: Targeted<HTMLInputElement>) {
+    const num = event.currentTarget.valueAsNumber;
+    const max = getMax();
+    const val = Math.max(0, Math.min(max, num));
+    el.scrollLeft = val;
+    setVal(val);
+  }
+
+  createEffect(() => {
+    onScroll();
+    const resize = new ResizeObserver(onScroll);
+    resize.observe(el);
+    const onWin = () => onScroll();
+    window.addEventListener("resize", onWin, { passive: true });
+    onCleanup(() => {
+      resize.disconnect();
+      window.removeEventListener("resize", onWin);
+    });
+  });
+
   return (
-    <ul role="marquee" {...parent}>
-      <For each={getChildren()}>{(child) => <li>{child}</li>}</For>
-    </ul>
+    <div>
+      <ul ref={el} role="marquee" onScroll={onScroll} {...parent}>
+        <For each={getChildren()}>{(child) => <li>{child}</li>}</For>
+      </ul>
+      <input
+        type="range"
+        min={0}
+        max={getMax()}
+        value={getVal()}
+        class="accent-primary w-full"
+        onInput={onInput}
+      />
+    </div>
   );
 }
