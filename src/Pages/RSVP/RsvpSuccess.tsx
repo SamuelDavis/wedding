659cfac00c6a757f003732bdfbebe60fb88aa5e4
Samuel Davis <samueljakdavis@gmail.com>
2026-02-21 23:21:05 -0500

    scroll to aside on success

--- src/Pages/RSVP/RsvpSuccess.tsx
+++ src/Pages/RSVP/RsvpSuccess.tsx
@@ -3,13 +3,24 @@ import RsvpLayout from "./RsvpLayout";
 import Arrow from "../../Components/Arrow";
 import Time from "../../Components/Time";
 import { venueMapHref, weddingDate } from "../../data";
+import { onMount } from "solid-js";
 
 export default function RsvpSuccess() {
+  let el: undefined | HTMLHeadingElement;
+  onMount(() => {
+    if (!el) return;
+    queueMicrotask(() => {
+      const y = el.getBoundingClientRect().bottom;
+      window.scrollTo({ top: y, behavior: "smooth" });
+    });
+  });
   return (
     <RsvpLayout>
       <section>
         <aside class="mb-(--gap-lg)">
-          <h1 class="mb-0">Success!</h1>
+          <h1 ref={el} class="mb-0">
+            Success!
+          </h1>
           <p>
             <span>See you on </span>
             <b>
